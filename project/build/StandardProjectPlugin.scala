b96935e1a64eeb08a54e37034627aa2077e96911
Robey Pointer <robey@twitter.com>
2010-05-03 22:20:06 -0700

    rename to StandardProjectPlugin.

--- project/build/StandardProjectPlugin.scala
+++ project/build/StandardProjectPlugin.scala
@@ -5,4 +5,5 @@ import sbt._
 class StandardProjectPlugin(info: ProjectInfo) extends PluginProject(info) {
   override def disableCrossPaths = true
   override def ivyCacheDirectory = Some(Path.fromFile(new File(System.getProperty("user.home"))) / ".ivy2-sbt" ##)
+  val publishTo = Resolver.sftp("green.lag.net", "green.lag.net", "/web/nest")
 }
