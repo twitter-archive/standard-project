b95e61e34cee5957c54c9f29530b36a5e9fb9554
Robey Pointer <robey@twitter.com>
2010-07-12 20:21:40 -0700

    add license.

--- project/build/StandardProjectPlugin.scala
+++ project/build/StandardProjectPlugin.scala
@@ -5,6 +5,18 @@ import sbt._
 class StandardProjectPlugin(info: ProjectInfo) extends PluginProject(info) {
   override def disableCrossPaths = true
   override def ivyCacheDirectory = Some(Path.fromFile(new File(System.getProperty("user.home"))) / ".ivy2-sbt" ##)
-  val publishTo = Resolver.sftp("green.lag.net", "green.lag.net", "/web/nest")
+
   val ivySvn = "ivysvn" % "ivysvn" % "2.1.0" from "http://www.lag.net/nest/ivysvn/ivysvn/2.1.0/ivysvn-2.1.0.jar"
+
+  override def pomExtra =
+    <licenses>
+      <license>
+        <name>Apache 2</name>
+        <url>http://www.apache.org/licenses/LICENSE-2.0.txt</url>
+        <distribution>repo</distribution>
+      </license>
+    </licenses>
+
+  Credentials(Path.userHome / ".ivy2" / "credentials", log)
+  val publishTo = "nexus" at "http://nexus.scala-tools.org/content/repositories/releases/"
 }
