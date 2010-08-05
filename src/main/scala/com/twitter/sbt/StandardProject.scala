f55954887ec2820ce2cad5011f71f692485d3947
mmcbride <mccv@twitter.com>
2010-08-04 17:26:57 -0700

    changed ivy svn thinger to be m2 compatible (for pom resolution).  Added a twitterPrivateM2 style repo so we can get transitive dependencies

--- src/main/scala/com/twitter/sbt/StandardProject.scala
+++ src/main/scala/com/twitter/sbt/StandardProject.scala
@@ -44,6 +44,12 @@ class StandardProject(info: ProjectInfo) extends DefaultProject(info) with Sourc
     Resolver.url("twitter-private") artifacts("file:/tmp/[artifact]-[revision].[ext]")
   }
 
+  val twitterPrivateRepoM2 = if (env.get("SBT_TWITTER").isDefined) {
+    new MavenRepository("twitter-private-m2", "http://binaries.local.twitter.com/maven/")
+  } else {
+    DefaultMavenRepository
+  }
+
   // make a build.properties file and sneak it into the packaged jar.
   def buildPackage = organization + "." + name
   def packageResourcesPath = buildPackage.split("\\.").foldLeft(mainResourcesOutputPath ##) { _ / _ }
