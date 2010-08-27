989fc683d035096f5a954412bd564645e3015f02
Robey Pointer <robey@twitter.com>
2010-08-26 17:20:27 -0700

    use ivysvn from github now.

--- project/build/StandardProjectPlugin.scala
+++ project/build/StandardProjectPlugin.scala
@@ -6,7 +6,7 @@ class StandardProjectPlugin(info: ProjectInfo) extends PluginProject(info) {
   override def disableCrossPaths = true
   override def ivyCacheDirectory = Some(Path.fromFile(new File(System.getProperty("user.home"))) / ".ivy2-sbt" ##)
 
-  val ivySvn = "ivysvn" % "ivysvn" % "2.1.0" from "http://www.lag.net/nest/ivysvn/ivysvn/2.1.0/ivysvn-2.1.0.jar"
+  val ivySvn = "ivysvn" % "ivysvn" % "2.1.0" from "http://twitter.github.com/repo/ivysvn/ivysvn/2.1.0/ivysvn-2.1.0.jar"
 
   override def pomExtra =
     <licenses>
