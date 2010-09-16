7977a37d374f23e4cde33bfd130b0c819f424737
Kyle Maxwell <kmaxwell@twitter.com>
2010-09-16 16:02:13 -0700

    correct deps

--- src/main/scala/com/twitter/sbt/CorrectDependencies.scala
+++ src/main/scala/com/twitter/sbt/CorrectDependencies.scala
@@ -3,11 +3,11 @@ package com.twitter.sbt
 import _root_.sbt._
 import org.apache.ivy.plugins._
 
-trait StrictDependencies extends BasicManagedProject { self: DefaultProject =>
+trait CorrectDependencies extends BasicManagedProject { self: DefaultProject =>
   override def ivySbt: IvySbt = {
     val i = super.ivySbt
     i.withIvy { apacheIvy =>
-      val stricty = apacheIvy.getSettings().getConflictManager("strict")
+      val stricty = apacheIvy.getSettings().getConflictManager("latest-compatible")
       stricty.asInstanceOf[IvySettingsAware].setSettings(apacheIvy.getSettings())
       apacheIvy.getSettings().setDefaultConflictManager(stricty)
     }
