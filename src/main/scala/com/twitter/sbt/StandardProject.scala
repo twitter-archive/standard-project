4e4afcb41c0adc09a3a2e771618e3ced3afa74d6
Robey Pointer <robey@twitter.com>
2010-09-21 14:22:11 -0700

    update version in the banner.

--- src/main/scala/com/twitter/sbt/StandardProject.scala
+++ src/main/scala/com/twitter/sbt/StandardProject.scala
@@ -172,5 +172,5 @@ class StandardProject(info: ProjectInfo) extends DefaultProject(info) with Sourc
   val cleanDist = cleanTask("dist" ##) describedAs("Erase any packaged distributions.")
   override def cleanAction = super.cleanAction dependsOn(cleanThrift, cleanDist)
 
-  log.info("Standard project rules 0.7.1 loaded (2010-09-03).")
+  log.info("Standard project rules 0.7.5 loaded (2010-09-21).")
 }
