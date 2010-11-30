46b7bd594f327fc34b4874d0022c4b8be8a7ddc8
Kyle Maxwell <kmaxwell@twitter.com>
2010-11-30 10:28:14 -0800

    fix namespace conflict

--- src/main/scala/com/twitter/sbt/InlineDependencies.scala
+++ src/main/scala/com/twitter/sbt/InlineDependencies.scala
@@ -5,7 +5,7 @@ import scala.collection.jcl
 import _root_.sbt._
 
 trait InlineDependencies extends BasicManagedProject { self: DefaultProject =>
-  val environment = jcl.Map(System.getenv())
+  val inlineEnvironment = jcl.Map(System.getenv())
   val inlinedLibraryDependencies = new HashSet[ModuleID]()
   val inlinedSubprojects = new ListBuffer[(String, _root_.sbt.Project)]()
 
@@ -19,7 +19,7 @@ trait InlineDependencies extends BasicManagedProject { self: DefaultProject =>
 
   def inline(m: ModuleID) = {
     val path = Path.fromFile("../" + m.name)
-    if (environment.get("SBT_INLINE").isDefined && path.isDirectory)
+    if (inlineEnvironment.get("SBT_INLINE").isDefined && path.isDirectory)
       inlinedSubprojects += (m.name -> project(path))
     else
       inlinedLibraryDependencies += m
