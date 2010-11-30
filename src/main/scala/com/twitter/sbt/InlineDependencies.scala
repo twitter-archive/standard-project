0e127df5b967ea1ba4ed3f2fb85a4812ae4ec3f2
Kyle Maxwell <kmaxwell@twitter.com>
2010-11-30 10:23:41 -0800

    inline deps, 0.7.15

---
+++ src/main/scala/com/twitter/sbt/InlineDependencies.scala
@@ -0,0 +1,27 @@
+package com.twitter.sbt
+
+import collection.mutable.{HashSet, ListBuffer}
+import scala.collection.jcl
+import _root_.sbt._
+
+trait InlineDependencies extends BasicManagedProject { self: DefaultProject =>
+  val environment = jcl.Map(System.getenv())
+  val inlinedLibraryDependencies = new HashSet[ModuleID]()
+  val inlinedSubprojects = new ListBuffer[(String, _root_.sbt.Project)]()
+
+  override def libraryDependencies = {
+    super.libraryDependencies ++ inlinedLibraryDependencies
+  }
+
+  override def subProjects = {
+    Map() ++ super.subProjects ++ inlinedSubprojects
+  }
+
+  def inline(m: ModuleID) = {
+    val path = Path.fromFile("../" + m.name)
+    if (environment.get("SBT_INLINE").isDefined && path.isDirectory)
+      inlinedSubprojects += (m.name -> project(path))
+    else
+      inlinedLibraryDependencies += m
+  }
+}
